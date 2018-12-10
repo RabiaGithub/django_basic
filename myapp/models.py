982977df72165037d6832c8f2189f335bc26adcf
RabiaGithub <you@example.com>
2018-12-10 15:51:29 +0530

    generating models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Register(models.Model):
+    name = models.CharField(max_length=30)
+    phone = models.IntegerField()
+    email = models.EmailField()
+    body = models.TextField()
+
+    def __str__(self):
+        return self.name
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from .forms import RegisterForm
+
 
 def index(request):
-    return render(request,'myapp/index.html')
+        if request.method == "POST":
+                form = RegisterForm(request.POST)
+                if form.is_valid():
+                        name = form.cleaned_data['name']
+                        email = form.cleaned_data['email']
+                        print(name,email)
+                        form.save()
+        form = RegisterForm()
+        return render(request,'formapp/index.html',{'form':form})
+
+def modal_detail(request):
+        return render(request,'formapp/form.html')
+
+
